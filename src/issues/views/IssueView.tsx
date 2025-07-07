5af626634ffcf7cd1d214223ef8f1a0779ba2bc5
julian-valencia-neostella <julian.valencia@neostella.com>
2025-07-07 09:08:49 -0500

    Added prefetchQuery for issues

--- src/issues/views/IssueView.tsx
+++ src/issues/views/IssueView.tsx
@@ -1,8 +1,8 @@
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
-import { IssueComment } from '../components/IssueComment';
-import { FiSkipBack } from 'react-icons/fi';
-import { useGetIssue } from '../hooks/useGetIssue';
-import LoadingSpinner from '../../shared/components/LoadingSpinner';
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { IssueComment } from "../components/IssueComment";
+import { FiSkipBack } from "react-icons/fi";
+import { useGetIssue } from "../hooks/useGetIssue";
+import LoadingSpinner from "../../shared/components/LoadingSpinner";
 
 export const IssueView = () => {
   const navigate = useNavigate();
@@ -12,22 +12,22 @@ export const IssueView = () => {
   const { issueQuery, issueCommentsQuery } = useGetIssue(issueNumber);
 
   if (issueQuery.isLoading) {
-    return <div className=''>Loading Issue...</div>;
+    return <div className="">Loading Issue...</div>;
   }
 
   if (!issueQuery.data) {
-    return <Navigate to='/404' />;
+    return <Navigate to="/404" />;
   }
 
   return (
-    <div className='mb-5'>
-      <div className='mb-4'>
+    <div className="mb-5">
+      <div className="mb-4">
         <button
           onClick={() => navigate(-1)}
-          className='hover:underline text-blue-400 flex items-center'
+          className="hover:underline text-blue-400 flex items-center"
         >
           <FiSkipBack />
-          Regresar
+          Back
         </button>
       </div>
 
